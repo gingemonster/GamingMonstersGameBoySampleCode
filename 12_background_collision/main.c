68147975155d7067841813834fd2f89bb588d356
gingemonster <brian@earthware.co.uk>
2019-02-08 21:49:23 +0000

    got basic game working

--- 12_background_collision/main.c
+++ 12_background_collision/main.c
@@ -4,7 +4,12 @@
 #include "MazeMap.c"
 #include "Snail.c"
 
+const char blankmap[1] = {0x00};
 UINT8 playerlocation[2];
+UBYTE haskey;
+UBYTE debug;
+UBYTE gamerunning = 1;
+
 
 void performantdelay(UINT8 numloops){
     UINT8 i;
@@ -33,17 +38,37 @@ UBYTE canplayermove(UINT8 newplayerx, UINT8 newplayery){
 
     indexBLx = (newplayerx - 8) / 8;
     indexBLy = (newplayery - 10) / 8;
-    tileindexBL = 20 * indexBLy + indexBLx;    
-   
-    // printf("%u %u\n",(UINT16)(newplayerx),(UINT16)(newplayery),(UINT16)indexTLx,(UINT16)indexTLy,(UINT16)tileindexTL);
-    // printf("%u %u %u\n",(UINT16)indexTLx,(UINT16)indexTLy,(UINT16)tileindexTL);
-    // printf("%u %u %u\n",(UINT16)indexBRx,(UINT16)indexBRy,(UINT16)tileindexBR);
+    tileindexBL = 20 * indexBLy + indexBLx; 
+
+    if(debug){
+
+        printf("%u %u\n",(UINT16)(newplayerx),(UINT16)(newplayery),(UINT16)indexTLx,(UINT16)indexTLy,(UINT16)tileindexTL);
+        printf("%u %u %u\n",(UINT16)indexTLx,(UINT16)indexTLy,(UINT16)tileindexTL);
+        printf("%u %u %u\n",(UINT16)indexBRx,(UINT16)indexBRy,(UINT16)tileindexBR);
+    }   
     
     // check if tile index, for top left and bottom right of sprite, in background map is empty (0x00);
     result = MazeMap[tileindexTL] == 0 && MazeMap[tileindexBR] == 0 && MazeMap[tileindexTR] == 0 && MazeMap[tileindexBL] == 0;
-    if(result == 0){
-        //printf("%u %u %u %u\n",(UINT16)tileindexTL,(UINT16)tileindexTR,(UINT16)tileindexBR,(UINT16)tileindexBL);
+
+    // check for the two special locations, key and door
+    if(tileindexBR==321){
+        // colect key
+        set_bkg_tiles(1, 16, 1, 1, blankmap);
+        MazeMap[321] = 0;
+        haskey = 1;
     }
+    else if(tileindexBR==263 && haskey){
+        // open door
+        set_bkg_tiles(3, 13, 1, 1, blankmap);
+        MazeMap[263] = 0;
+    }
+    else if (tileindexBR==340){
+        // finish game
+        gamerunning = 0;
+        HIDE_SPRITES;
+        printf("\n \n \n \n \n \n \n \n \n      YOU WIN!");
+    }
+
     return result;
 }
 
@@ -64,8 +89,10 @@ void main(){
     SHOW_BKG;
     DISPLAY_ON;
 
-    while(1){
-
+    while(gamerunning){
+        if(joypad() & J_A){
+            debug = 1;
+        }
         if(joypad() & J_LEFT){
             if(canplayermove(playerlocation[0]-1,playerlocation[1])){
                 playerlocation[0] -= 1;
