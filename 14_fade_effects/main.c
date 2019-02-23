e3158c2c8a9d7f0343f0586c6722a96d0fdb522f
gingemonster <brian@earthware.co.uk>
2019-02-23 16:36:23 +0000

    updating readme and adding code for next tutorial

---
+++ 14_fade_effects/main.c
@@ -0,0 +1,67 @@
+#include <gb/gb.h>
+#include <stdio.h>
+#include "mario_data.c"
+#include "mario_map.c"
+#include "MazeSprites.c"
+#include "MazeMap.c"
+UINT8 i;
+
+void performantdelay(UINT8 numloops){
+    UINT8 ii;
+    for(ii = 0; ii < numloops; ii++){
+        wait_vbl_done();
+    }     
+}
+
+void fadeout(){
+    for(i=0;i!=4;i++){
+		switch(i){
+			case 0:
+				BGP_REG = 0xE4;
+				break;
+			case 1:
+				BGP_REG = 0xF9;
+				break;
+			case 2:
+				BGP_REG = 0xFE;
+				break;
+			case 3:
+				BGP_REG = 0xFF;
+				break;												
+		}
+		performantdelay(50);
+	}	
+}
+
+void fadein(){
+	for(i=1;i!=4;i++){
+		switch(i){
+			case 1:
+				BGP_REG = 0xFE;
+				break;
+			case 2:
+				BGP_REG = 0xF9;
+				break;
+			case 3:
+				BGP_REG = 0xE4;
+				break;											
+		}
+		performantdelay(50);
+	}	
+}
+
+void main(){
+    set_bkg_data(0, 114, mario_data);
+    set_bkg_tiles(0, 0, 20, 18, mario_map);
+
+    SHOW_BKG;
+    DISPLAY_ON;
+
+    waitpad(J_START);
+    fadeout();
+
+    set_bkg_data(0, 4, MazeSprites);
+    set_bkg_tiles(0, 0, 20, 18, MazeMap);  
+
+    fadein();
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # GamingMonstersGameBoySampleCode
-Sample code to accompany our youtube series https://www.youtube.com/watch?v=HIsWR_jLdwo&amp;list=PLeEj4c2zF7PaFv5MPYhNAkBGrkx4iPGJo
+Sample code to accompany our youtube series https://www.youtube.com/playlist?list=PLeEj4c2zF7PaFv5MPYhNAkBGrkx4iPGJo
