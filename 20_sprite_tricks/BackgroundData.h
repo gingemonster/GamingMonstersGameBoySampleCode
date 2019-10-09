25950e5c290b9076a7ad3da256dee8f7d15d3bb6
gingemonster <brian@earthware.co.uk>
2019-10-09 20:32:09 +0100

    finishing tutorial 20

--- 20_sprite_tricks/BackgroundData.h
+++ 20_sprite_tricks/BackgroundData.h
@@ -1,6 +1,6 @@
 /*
 
- EXAMPLESPRITE.H
+ BACKGROUNDDATA.H
 
  Include File.
 
@@ -10,7 +10,7 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 0
+  Tiles                : 0 to 3
 
   Palette colors       : None.
   SGB Palette          : None.
@@ -24,8 +24,8 @@
 
 
 /* Bank of tiles. */
-#define ExampleSpriteBank 0
+#define BackgroundDataBank 0
 /* Start of tile array. */
-extern unsigned char ExampleSprite[];
+extern unsigned char BackgroundData[];
 
-/* End of EXAMPLESPRITE.H */
+/* End of BACKGROUNDDATA.H */
--- 20_sprite_tricks/main.c
+++ 20_sprite_tricks/main.c
@@ -1,26 +1,61 @@
 #include <gb/gb.h>
 #include <stdio.h>
 #include "ExampleSprite.c"
+#include "BackgroundData.c"
+#include "BackgroundMap.c"
+
+void performantdelay(UINT8 numloops){
+    UINT8 i;
+    for(i = 0; i < numloops; i++){
+        wait_vbl_done();
+    }     
+}
 
 void main(){
-    UINT8 currentspriteindex = 0;
+
+    set_bkg_data(0,4,BackgroundData);
+    set_bkg_tiles(0,0,20,18,BackgroundMap);
 
     set_sprite_data(0, 2, ExampleSprite);
+
     set_sprite_tile(0, 0);
     set_sprite_tile(1, 0);
     set_sprite_tile(2, 0);
     set_sprite_tile(3, 0);
+    set_sprite_tile(4, 0);
+
     move_sprite(0, 22, 78);
     move_sprite(1, 44, 78);
     move_sprite(2, 66, 78);
     move_sprite(3, 88, 78);
+    move_sprite(4, 26, 59);
 
     set_sprite_prop(1,S_FLIPX);
     set_sprite_prop(2,S_FLIPY);
     set_sprite_prop(3,S_FLIPX | S_FLIPY);
+
+    set_sprite_prop(4,S_PRIORITY | S_FLIPX);
+
     SHOW_SPRITES;
+    SHOW_BKG;
 
     while(1){
-        
+        switch(joypad()){
+            case J_LEFT: 
+                scroll_sprite(4,-1,0);
+                break;
+            case J_RIGHT: 
+                scroll_sprite(4,1,0);
+                break;  
+            case J_UP: 
+                scroll_sprite(4,0,-1);
+
+                set_sprite_prop(4,get_sprite_prop(4) & ~S_PRIORITY);
+                break; 
+            case J_DOWN: 
+                scroll_sprite(4,0,1);
+                break;                                                   
+        }
+        performantdelay(2);
     }
 }
