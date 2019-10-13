dbbf4117f0aa57dbf657f646120808cefc7b8f4e
gingemonster <brian@earthware.co.uk>
2019-10-13 10:00:20 +0100

    tutorial 21 code update

--- 21_parallax_scrolling/main.c
+++ 21_parallax_scrolling/main.c
@@ -1,35 +1,33 @@
 #include <gb/gb.h>
 #include "parallax_background_data.c"
 #include "parallax_background_map.c"
-UINT16 backgroundoffset1x;
-UINT16 backgroundoffset2x;
-UINT16 backgroundoffset3x;
-
-void interuptLCD(){
-    switch(LYC_REG){
-        case 0x00: // at top
-            move_bkg(backgroundoffset1x,0);
-            disable_interrupts();
-            LYC_REG = 0x35;
-            enable_interrupts();
-            break;        
-        case 0x35: // at mountains
-            move_bkg(backgroundoffset2x,0);
-            LYC_REG = 0x6c;
-            break;
-        case 0x6c: // at trees
-            
-            move_bkg(backgroundoffset3x,0);
-            LYC_REG = 0x00;
-            break;
-    }
-}
+UINT8 backgroundoffset1x;
+UINT8 backgroundoffset2x;
+UINT8 backgroundoffset3x;
 
 void performantdelay(UINT8 numloops){
     UINT8 i;
     for(i = 0; i < numloops; i++){
         wait_vbl_done();
     }     
+}
+
+void interruptLCD(){
+    switch (LYC_REG)
+    {
+        case 0x00:
+            move_bkg(backgroundoffset1x,0);
+            LYC_REG = 0x35;
+            break;
+        case 0x35:
+            move_bkg(backgroundoffset2x,0);
+            LYC_REG = 0x6c;
+            break;
+        case 0x6c:
+            move_bkg(backgroundoffset3x,0);
+            LYC_REG = 0x00;
+            break;            
+    }
 }
 
 void main(){
@@ -40,14 +38,15 @@ void main(){
     set_bkg_data(0, 189, parallax_background_data);
     set_bkg_tiles(0, 0, 32, 18, parallax_background_map);
 
-    STAT_REG = 0x45; // enable LYC=LY interrupt so that we can set a specific line it will fire att
-    LYC_REG = 0x00;  // first fire LCD Interupt on the 1st scan line    
+    STAT_REG = 0x45; // enable LYC=LY interrupt so that we can set a specific line it will fire at
+    LYC_REG = 0x00;
 
     disable_interrupts();
-    add_LCD(interuptLCD);
+    add_LCD(interruptLCD);
     enable_interrupts();
-    
+
     set_interrupts(VBL_IFLAG | LCD_IFLAG);
+
     SHOW_BKG;
     DISPLAY_ON;
 
