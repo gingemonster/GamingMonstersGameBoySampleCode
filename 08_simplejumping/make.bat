81c9b2ffc1f057291bdaaaf490ef37914b3bc4fd
gingemonster <brian@earthware.co.uk>
2020-09-03 08:01:45 +0100

    putting gbdk path back to default

--- 08_simplejumping/make.bat
+++ 08_simplejumping/make.bat
@@ -1,2 +1,2 @@
-c:\gbdk2020\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c
-c:\gbdk2020\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb main.o
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb main.o
